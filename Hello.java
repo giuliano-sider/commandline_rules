449304d63b9f69a03c684833dd248de79ea9d4be
Giuliano Sider <giuliano.sider@gmail.com>
2015-08-27 23:26:00 -0300

    this aint yo mama's c++

--- Hello.java
+++ Hello.java
@@ -5,9 +5,9 @@ public class Hello {
 
 	public static void main(String[] args){
 
-		int a = new int(42);
+		/*int a = new int(42); this aint yo mamma's c++ */
 		System.out.println("hi\n");
-		
+
 	}
 
 }
